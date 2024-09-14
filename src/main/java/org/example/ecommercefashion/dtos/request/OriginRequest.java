2567c15776a0ffd41712a4e95af891eab7754159
mainghiep <nghiepmai2004@gmail.com>
2024-09-14 14:14:04 +0700

    them table origin

---
+++ src/main/java/org/example/ecommercefashion/dtos/request/OriginRequest.java
@@ -0,0 +1,15 @@
+package org.example.ecommercefashion.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class OriginRequest {
+
+    private String name;
+
+}
---
+++ src/main/java/org/example/ecommercefashion/services/impl/OriginServiceImpl.java
@@ -0,0 +1,108 @@
+package org.example.ecommercefashion.services.impl;
+
+import com.longnh.exceptions.ExceptionHandle;
+import com.longnh.utils.FnCommon;
+import lombok.RequiredArgsConstructor;
+import org.example.ecommercefashion.dtos.filter.OriginParam;
+import org.example.ecommercefashion.dtos.request.OriginRequest;
+import org.example.ecommercefashion.dtos.response.JwtResponse;
+import org.example.ecommercefashion.dtos.response.MessageResponse;
+import org.example.ecommercefashion.dtos.response.OriginResponse;
+import org.example.ecommercefashion.dtos.response.ResponsePage;
+import org.example.ecommercefashion.dtos.response.UserResponse;
+import org.example.ecommercefashion.entities.Origin;
+import org.example.ecommercefashion.entities.User;
+import org.example.ecommercefashion.exceptions.ErrorMessage;
+import org.example.ecommercefashion.repositories.OriginRepository;
+import org.example.ecommercefashion.repositories.UserRepository;
+import org.example.ecommercefashion.services.OriginService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class OriginServiceImpl implements OriginService{
+
+    private final OriginRepository repository;
+
+    private final UserRepository userRepository;
+
+    private final org.example.ecommercefashion.security.JwtService JwtService;
+
+    @Override
+    public ResponsePage<Origin, OriginResponse> filterOrigin(OriginParam param, Pageable pageable){
+        Page<Origin> originPage = repository.FilterOrigin(param,pageable);
+        return new ResponsePage<>(originPage,OriginResponse.class);
+    }
+    @Override
+    public OriginResponse add(OriginRequest request, String token) {
+        if (token != null) {
+            JwtResponse jwt = JwtService.decodeToken(token);
+            Origin origin = new Origin();
+            FnCommon.copyNonNullProperties(origin,request);
+            origin.setCreateBy(jwt.getUserId());
+            origin = repository.save(origin);
+            OriginResponse response = new OriginResponse();
+            FnCommon.copyNonNullProperties(response,origin);
+            return response;
+        } else {
+            throw new ExceptionHandle(HttpStatus.BAD_REQUEST, ErrorMessage.ORIGIN_NOT_FOUND);
+        }
+    }
+    @Override
+    public OriginResponse getByOriginId(Long id) {
+        Origin origin = repository.findById(id).orElseThrow(
+                () -> new ExceptionHandle(HttpStatus.NOT_FOUND, ErrorMessage.ORIGIN_NOT_FOUND)
+        );
+        OriginResponse response = new OriginResponse();
+        FnCommon.copyNonNullProperties(response, origin);
+        if (origin.getCreateBy() != null) {
+            response.setCreateBy(getInfoUser(origin.getCreateBy()));
+        }
+        if (origin.getUpdateBy() != null) {
+            response.setUpdateBy(getInfoUser(origin.getUpdateBy()));
+        }
+        return response;
+    }
+    @Override
+    public OriginResponse update(OriginRequest request, Long id, String token) {
+        if (token != null) {
+            JwtResponse jwt = JwtService.decodeToken(token);
+            Origin origin = repository.findById(id).orElseThrow(
+                    () -> new ExceptionHandle(HttpStatus.NOT_FOUND, ErrorMessage.ORIGIN_NOT_FOUND)
+            );
+            FnCommon.copyNonNullProperties(origin,request);
+            origin.setUpdateBy(jwt.getUserId());
+            origin = repository.save(origin);
+            OriginResponse response = new OriginResponse();
+            FnCommon.copyNonNullProperties(response,origin);
+            return response;
+
+        } else {
+            throw new ExceptionHandle(HttpStatus.BAD_REQUEST, ErrorMessage.INVALID_REFRESH_TOKEN);
+        }
+    }
+    @Override
+    public MessageResponse deleted(Long id) {
+        Origin origin = repository.findById(id).orElseThrow(
+                () -> new ExceptionHandle(HttpStatus.NOT_FOUND, ErrorMessage.ORIGIN_NOT_FOUND)
+        );
+
+        origin.setDeleted(true);
+        repository.save(origin);
+
+        return MessageResponse.builder().message("Origin deleted successfully").build();
+    }
+    private UserResponse getInfoUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() ->
+                new ExceptionHandle(HttpStatus.NOT_FOUND, ErrorMessage.USER_NOT_FOUND)
+        );
+        UserResponse userResponse = new UserResponse();
+        FnCommon.copyNonNullProperties(userResponse, user);
+        return userResponse;
+
+    }
+}

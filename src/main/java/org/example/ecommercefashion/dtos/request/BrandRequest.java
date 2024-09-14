5e83d8310857e1245e76041def8eb7b540c17b6e
mainghiep <nghiepmai2004@gmail.com>
2024-09-14 15:21:07 +0700

    ok luon

--- src/main/java/org/example/ecommercefashion/dtos/request/BrandRequest.java
+++ src/main/java/org/example/ecommercefashion/dtos/request/BrandRequest.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BrandRequest {
-
+    @NotBlank(message = "name is required")
+    @Size(min = 2, max = 50, message = "name must be between 2 and 50 characters")
     private String name;
 
 }
--- src/main/java/org/example/ecommercefashion/dtos/request/OriginRequest.java
+++ src/main/java/org/example/ecommercefashion/dtos/request/OriginRequest.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class OriginRequest {
-
+    @NotBlank(message = "name is required")
+    @Size(min = 2, max = 50, message = "name must be between 2 and 50 characters")
     private String name;
 
 }

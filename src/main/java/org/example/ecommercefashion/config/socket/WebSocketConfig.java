11d0cf7d36e191ce378f5b3160ee1a735e38d7f2
Trần Minh Thái <thaitranminh564@gmail.com>
2024-09-21 18:41:10 +0700

    config new domain for websocket

--- src/main/java/org/example/ecommercefashion/config/socket/WebSocketConfig.java
+++ src/main/java/org/example/ecommercefashion/config/socket/WebSocketConfig.java
@@ -17,7 +17,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins(
+                        "http://localhost:5173",
+                        "http://ecommerce-fashion.site"
+                )
                 .withSockJS();
     }
 

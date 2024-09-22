b03b4624c9d079df8b9c06c47dea161a0802c743
Long23112002 <115551484+Long23112002@users.noreply.github.com>
2024-09-22 13:39:38 +0700

    push

--- services/images/migrations/V1_0_0__Create_table_image.sql
+++ services/images/migrations/V1_0_0__Create_table_image.sql
@@ -1,16 +1,10 @@
-
-
-CREATE TYPE image_type_enum AS ENUM ('JPG', 'PNG', 'SVG');
-
-CREATE TABLE images.images
+CREATE TABLE IF NOT EXISTS images.images
 (
-    id         BIGSERIAL PRIMARY KEY,
-    url        VARCHAR(255) NOT NULL,
-    type       image_type_enum NOT NULL,
-    size       BIGINT NOT NULL,
-    title      VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMPTZ,
-    deleted    BOOLEAN NOT NULL DEFAULT FALSE
+    id          SERIAL PRIMARY KEY,
+    url         TEXT         NOT NULL,
+    object_id   BIGINT       NOT NULL,
+    object_name varchar(100) NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted     BOOLEAN   DEFAULT FALSE
 );

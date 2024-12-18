8cbe45d2de3acbb6d28c65f67503eb8799709f54
longnh <longjava2024@gmail.com>
2024-12-18 23:34:53 +0700

    fix dockerfile

--- src/main/java/org/example/ecommercefashion/services/impl/QRCodeGeneratorServiceImpl.java
+++ src/main/java/org/example/ecommercefashion/services/impl/QRCodeGeneratorServiceImpl.java
@@ -40,6 +40,7 @@ public class QRCodeGeneratorServiceImpl implements QRCodeGeneratorService {
       document.close();
       return pdfStream.toByteArray();
     } catch (Exception e) {
+      e.printStackTrace();
       throw new RuntimeException("Failed to generate QR code", e);
     }
   }
